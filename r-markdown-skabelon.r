7a23b7102e7d40a44bd696d9a7769bb1b91e3c7a
filippignatov <ignatov.fil@gmail.com>
2026-03-24 14:50:42 +0000

    Kaggle Notebook | R Markdown Skabelon | Version 7

--- r-markdown-skabelon.r
+++ r-markdown-skabelon.r
@@ -1,16 +1,20 @@
 ---
-title: "R Markdown Analysis"
-author: "R Coder"
-date: "2026-03-24"
+title: "R Markdown Mastery"
+author: "Data Scientist"
+date: "`r Sys.Date()`"
 output:
   html_document:
     number_sections: true
     toc: true
+    theme: cosmo
 ---
 
-# Introduktion
-Dette er et R Markdown script.
+# 1. Introduktion
+Dette script er genereret til at demonstrere **R Markdown** workflowet på Kaggle-platformen. Formålet er at opnå "R Markdown Coder" badget gennem metodisk dokumentation og visualisering.
 
-```{r setup, include=FALSE}
+# 2. Opsætning og Biblioteker
+Her indlæser vi de nødvendige værktøjer. Vi bruger **tidyverse** til effektiv datamanipulation.
+
+```{r load_libraries, message=FALSE, warning=FALSE}
 library(tidyverse)
-```
+
